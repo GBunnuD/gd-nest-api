b848ca0104a358b202b158b074df79f4fd7ece82
GD-Prasad <g.d.prasad004@gmail.com>
2021-02-11 21:05:30 -0600

    location and user entity add and ormconfig.js file add

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestModule } from './quest/quest.module';
+import { LocationModule } from './location/location.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [QuestModule],
+  imports: [QuestModule, LocationModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
